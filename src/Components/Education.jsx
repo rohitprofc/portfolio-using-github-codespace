4556668955e3d7f5c3ed20dded8d01f1c78104eb
Rohit <100615505+rohitprofc@users.noreply.github.com>
2025-02-06 19:58:07 +0000

    Add Education component and update navigation links

---
+++ src/Components/Education.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          padding: "3rem",
+        }}
+      >
+        B.Tech in Aritificial Inteligence and Data Science
+      </div>
+    </section>
+  );
+};
+
+export default Education;
