f75fb36cb58aa19a217ccf567b14614dd8127c2f
Rohit <100615505+rohitprofc@users.noreply.github.com>
2025-02-06 18:42:47 +0000

    Update personal information and images in Home, About, Portfolio components; modify project descriptions in Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Rohit Kavuluri",
+  title: "Web Developer & AI Engineer",
+  email: "rohitprofc@gmail.com",
+  gitHub: "rohitprofc",
+  instagram: "rohitprofc",
+  linkedIn: "rohitprofc",
+  medium: "rohitprofc",
+  twitter: "rohitprofc",
+  youTube: "rohitprofc",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/computer-desk.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -31,28 +31,26 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Visual Search Engine for Jewellery using AI",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "To provide customers a productive and engaging tool of finding similar jewellery by using images instead of keywords for faster and efficient search.",
+    url: "",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Edvise App",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Mentor-Mentee Platform for students to connect with mentors and get guidance on their career path.",
+    url: "",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Portfolio Website",
+    description: "Personal Portfolio Website to showcase my skills and projects.",
+    url: "",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "AI Apointment Booking System",
+    description: "AI based appointment booking system for doctors and patients.",
+    url: "",
   },
 ];
 
